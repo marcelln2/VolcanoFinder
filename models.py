4dc9b66a3d9c0d7a27e93aaf37d1409c8efac08a
unstoppable_force <marcell.nemes2@gmail.com>
2025-10-17 15:10:42 +0200

    Fixed structure of the CNN, everything works now.

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class MyFirstCNN(nn.Module):
         x = self.pool2(torch.relu(self.bn2(self.conv2(x))))
         x = torch.flatten(x, 1)
         x = self.fc1(x)
-        #x = self.dropout1(x)
-        x = torch.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = torch.relu(x)
         x = torch.sigmoid(self.fc2(x))
         return x
